be87ebbc832e23643b5a23fc3d461c0db6467617
Boqorabdalla <boqorapps@gmail.com>
2026-04-26 11:50:34 +0300

    Fix missing Log facade import in AgentInventoryController

--- app/Http/Controllers/Agent/AgentInventoryController.php
+++ app/Http/Controllers/Agent/AgentInventoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Inventory;
 use App\Models\BeautyListing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
 class AgentInventoryController extends Controller
