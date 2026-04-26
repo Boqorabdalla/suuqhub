077a74bcd1c9e109a8f0b0f77bd53d2b1171b773
Boqorabdalla <boqorapps@gmail.com>
2026-04-26 14:31:01 +0300

    Fix agent shop orders view

--- resources/views/agent/shop/orders/index.blade.php
+++ resources/views/agent/shop/orders/index.blade.php
@@ -7,10 +7,6 @@
     <div class="page-content">
         <div class="container-fluid">
             <div class="row">
-<div class="main-content">
-    <div class="page-content">
-        <div class="container-fluid">
-            <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                         <h4 class="mb-sm-0">{{ get_phrase('Shop Orders') }}</h4>
