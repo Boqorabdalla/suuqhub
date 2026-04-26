70e24a36cbf9d92aaa99c0eb049144701b03a5bd
Boqorabdalla <boqorapps@gmail.com>
2026-04-26 15:12:21 +0300

    Revert to working navigation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -53,7 +53,7 @@
             @if(auth()->check() && auth()->user()->role == 3)
                 @include('admin.order_manager_navigation')
             @elseif(auth()->check() && auth()->user()->role == 2)
-                @include('user.navigation_sidebar')
+                @include('user.navigation')
             @else
                 @include('admin.navigation')
             @endif
